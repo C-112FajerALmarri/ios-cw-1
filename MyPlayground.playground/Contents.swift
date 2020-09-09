e31ec8175dc2ec0961664bfbe229133c9826a43f
fajer <Bakolaalm@gmail.com>
2020-09-09 20:59:18 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+var name = "fajer"
+var hoppy1 = " drawing "
+var hoppy2 = "handBall"
+var height = " 152 "
+var age = 15
+var firendName = " salma "
+var friendHeight = "150 "
+var friendAge = 15
+var frindHobby1 = " singing "
+var frindHobby2 = "reading "
+var age_difference = (age) - (friendAge)
+print(frindHobby1)
+print("my name is \(name)")
